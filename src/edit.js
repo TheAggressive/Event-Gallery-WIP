f50ced94eaf54c8d3b7ef566b3abc9ab15a852fe
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2024-08-18 18:24:43 +0000

    Update: Working on structure

--- src/edit.js
+++ src/edit.js
@@ -26,11 +26,10 @@ export default function Edit(props) {
         {hasImages && (
           <div className='wp-block-event-gallery columns-10'>
             {props.attributes.images.map((image, index) => {
+              console.log(image);
               return (
                 <figure key={image.id} className='wp-block-event-gallery-item'>
-                  <a href={image.url}>
-                    <img class={`wp-image-${image.id}`} key={index} loading='lazy' data-link={image.link} data-id={image.id} src={image.sizes.thumbnail.url} />
-                  </a>
+                  <img className={`wp-image-${image.id}`} key={index} loading='lazy' data-link={image.link} data-id={image.id} src={image.sizes.medium.url} />
                 </figure>
               );
             })}
